9550c4132806b675d85f9c32bf896ee77384f25c
emi-hi <emily.hillier@gmail.com>
2019-09-18 20:35:01 +0000

    counts letters in a string and returns object with counts

---
+++ countLetters.js
@@ -0,0 +1,21 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${actual} !=== ${expected}`);
+  }
+};
+
+const countLetters = function(inputLetters) {
+  let letterCount = {};
+  for (const letter of inputLetters) {
+    if (letter !== ' ') {
+      if (letterCount[letter]) {
+        letterCount[letter] += 1;
+      } else {
+        letterCount[letter] = 1;
+      }
+    }
+  }
+  return letterCount;
+};
