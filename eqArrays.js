ef9fd071389844ff576e4fa96831092d2ae9fc3b
emi-hi <emily.hillier@gmail.com>
2019-09-23 15:50:25 +0000

    refactored, added module

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌ Assertion Failed: ${actual} !=== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const eqArrays = function(actual, expected) {
   let result;
@@ -34,7 +28,6 @@ const eqArrays = function(actual, expected) {
   return result;
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true); // => true
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false);// => false
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false); // => false
+
+module.exports = eqArrays;
+
