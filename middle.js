dd56b933e1a5d341b2bafc56838ec0ce4148ff8e
emi-hi <emily.hillier@gmail.com>
2019-09-23 16:02:57 +0000

    added module

--- middle.js
+++ middle.js
@@ -1,26 +1,3 @@
-const eqArrays = function(actual, expected) {
-  if (actual.length === expected.length) {
-    for (let i = 0; i < actual.length; i ++) {
-      if (actual[i] !== expected[i]) {
-        return false;
-      }
-    }
-  } else {
-    return false;
-  } return true;
-};
-
-const assertArraysEqual = function(actual, expected) {
-  const trueOrFalse = eqArrays(actual, expected);
-  let outputString;
-  if (trueOrFalse) {
-    outputString = `✅ ${actual} and ${expected} are the same!`;
-  } else {
-    outputString = `❌ ${actual} and ${expected} are not the same!`;
-  }
-  console.log(outputString);
-};
-
 const middle = function(array) {
   let middleOutput = [];
   if (array.length > 2) {
@@ -36,6 +13,6 @@ const middle = function(array) {
   return middleOutput;
 };
 
-let arr1 = [1,2,3,4];
-let arr2 = [0,1,2,3,4];
-assertArraysEqual(middle(arr1), middle(arr2));
+
+module.exports = middle;
+
